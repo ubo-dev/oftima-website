28b7524e41fb207d4cffe6dd232e42553135830b
Umut Bilal Okur <okur.umutbilal@gmail.com>
2024-06-09 23:53:36 +0300

    initial commit

---
+++ src/app/layout.js
@@ -0,0 +1,20 @@
+import "./globals.css";
+import { DM_Sans } from "next/font/google";
+import ThemeProvider from "@/components/ThemeProvider";
+
+const dmSans = DM_Sans({ subsets: ["latin"], weight: ["400", "500", "700"] });
+
+export const metadata = {
+  title: "Oftima",
+  description: "Oftima Website",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={dmSans.className}>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  );
+}
